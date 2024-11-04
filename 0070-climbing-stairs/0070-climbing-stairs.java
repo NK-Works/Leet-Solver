403b51a9b594a47e208a4d23d49e6ed2d9fe75f3
Anneshu Nag <132702983+NK-Works@users.noreply.github.com>
2024-11-04 14:43:15 +0530

    Time: 0 ms (100%), Space: 40.9 MB (6.27%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,18 +1,25 @@
 class Solution {
     public int climbStairs(int n) {
         int dp[] = new int[n+1];
-        return climbStairsHelper(n, dp);
-    }
+        // return climbStairsHelper(n, dp);
 
-    public int climbStairsHelper(int n, int dp[]) {
-        if (n == 0) return 1;
-        if (n < 0) return 0;
-
-        if (dp[n] != 0) {
-            return dp[n];
+        dp[0] = dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
         }
-
-        dp[n] = climbStairsHelper(n-1, dp) + climbStairsHelper(n-2, dp);
         return dp[n];
+
     }
+
+    // public int climbStairsHelper(int n, int dp[]) {
+    //     if (n == 0) return 1;
+    //     if (n < 0) return 0;
+
+    //     if (dp[n] != 0) {
+    //         return dp[n];
+    //     }
+
+    //     dp[n] = climbStairsHelper(n-1, dp) + climbStairsHelper(n-2, dp);
+    //     return dp[n];
+    // }
 }
