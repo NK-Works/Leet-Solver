1d066f09a48ad46aa1b02e0185f4b34ae5f35d8a
Anneshu Nag <132702983+NK-Works@users.noreply.github.com>
2025-01-27 14:48:27 +0530

    Time: 75 ms (45.32%), Space: 45 MB (85.72%) - LeetHub

---
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
@@ -0,0 +1,6 @@
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        String doubleString = s + s;
+        return doubleString.substring(1, doubleString.length() - 1).contains(s);
+    }
+}
